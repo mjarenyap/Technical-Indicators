2d8b384249f310b0c4a241de5aec2f7661fde316
mjarenyap <mjaren.yap@gmail.com>
2019-06-10 16:27:11 +0800

    Bollinger Bands implemented

--- indicators.py
+++ indicators.py
@@ -1,4 +1,19 @@
 import talib
+import numpy as np
+
+def bollinger_bands(stock, data):
+	stock_series = data.history(stock, fields="price", bar_count=200, frequency='1d')
+	middle_band = stock_series.mean()
+	upper_band = middle_band + np.std(stock_series) * 2
+	lower_band = middle_band - np.std(stock_series) * 2
+
+	'''
+	BOLLINGER BANDS algorithm
+	if stock_series[-1] > upper_band: BUY
+	elif stock_series[-1] < lower_band: SELL
+	'''
+
+	return [upper_band, lower_band]
 
 def ema_vs_ema(stock, data):
 	stock_series = data.history(stock, fields="price", bar_count=200, frequency='1d')
@@ -11,7 +26,7 @@ def ema_vs_ema(stock, data):
 	short_ema[-1] < long_ema[-1]: SELL
 	'''
 
-	return [long_ema, short_ema]
+	return [long_ema[-1], short_ema[-1]]
 
 def rsi(stock, data):
 	period = 14
@@ -33,7 +48,7 @@ def rsi(stock, data):
 
 	return rsi_result
 
-def obv(stock, data):
+def obv(stock, context, data):
 	# IMPORTANT: Please set context.curr_obv = 0 in the initialize()
 
 	period = 2 # getting the PREVIOUS and CURRENT status of the stock
